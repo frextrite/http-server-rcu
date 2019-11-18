8a2787a24c3ecfa7256e162294210c06225863ab
Amol Grover <frextrite@gmail.com>
2019-11-18 19:21:06 +0530

    Add initial structures for the program
    Signed-off-by: Amol Grover <frextrite@gmail.com>

---
+++ http_server_rcu.c
@@ -0,0 +1,53 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/list.h>
+#include <linux/rculist.h>
+
+struct state {
+	bool is_in_recovery;
+};
+
+struct time {
+	int time;
+};
+
+struct client {
+	int id;
+	struct list_head __rcu *clients_list;
+};
+
+struct headers {
+	bool cors;
+	int content_type;
+	int timeout;
+};
+
+struct server {
+	struct list_head	__rcu	*clients;
+	struct headers		__rcu	*headers;
+	struct state		__rcu	*state;
+	struct time		__rcu	*update_timestamp;
+} server;
+
+inline int initialize_headers(void) {
+}
+
+inline int initialize_server(void) {
+	INIT_LIST_HEAD_RCU(server.clients);
+}
+
+static int __init http_server_rcu_init(void) {
+	if(!initialize_server()) {
+		return -EFAULT;
+	}
+	printk(KERN_ERR "Initializing server!");
+	return 0;
+}
+
+static void __exit http_server_rcu_exit(void) {
+	printk(KERN_ERR "Destroying server!");
+}
+
+module_init(http_server_rcu_init);
+module_exit(http_server_rcu_exit);
